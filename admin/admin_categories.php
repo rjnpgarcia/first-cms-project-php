bb4d6fddf1d441039c4e140244e10c2b57b217ad
rjnpgarcia <102405417+rjnpgarcia@users.noreply.github.com>
2022-08-12 23:39:04 +0800

    delete category query

--- admin/admin_categories.php
+++ admin/admin_categories.php
@@ -56,8 +56,16 @@
                 </div>
                 <div class="col-xs-6">
                     <?php
+                    // FIND ALL CATEGORIES QUERY
                     $query = "SELECT * FROM categories";
                     $select_categories = mysqli_query($connection, $query);
+
+                    // DELETE QUERY
+                    if (isset($_GET['delete'])) {
+                        $cat_id_delete = $_GET['delete'];
+                        $query = "DELETE FROM categories WHERE cat_id = $cat_id_delete";
+                        header("Location: admin_categories.php");
+                    }
                     ?>
                     <table class="table table-bordered table-hover">
                         <thead>
@@ -71,8 +79,11 @@
                             while ($row = mysqli_fetch_assoc($select_categories)) {
                                 $cat_id = $row['cat_id'];
                                 $cat_title = $row['cat_title'];
-                                echo "<tr><td>$cat_id</td><td>$cat_title</td></tr>";
+                                echo "<tr><td>$cat_id</td>";
+                                echo "<td>$cat_title</td>";
+                                echo "<td><a href='admin_categories.php?delete=$cat_id'>Delete</a></td></tr>";
                             }
+
                             ?>
                         </tbody>
                     </table>
--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,3 +1,5 @@
+<?php ob_start(); ?>
+
 <!-- Database Connection -->
 <?php include "../includes/db.php"; ?>
 
